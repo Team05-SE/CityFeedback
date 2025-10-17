6dbf955c32546db8ca026ec2ca0c0110eaff382d
maliceatthepalace <93584243+maliceatthepalace@users.noreply.github.com>
2025-10-17 14:37:06 +0200

    Behebung Example Test Klasse add src package

--- src/ExampleTest.java
+++ src/ExampleTest.java
@@ -1,5 +1,13 @@
-public class ExampleTest {
-    public static void main(String[] args) {
+package src;
+
+/**
+ * Eine einfache Testklasse für CI.
+ */
+public final class ExampleTest {
+
+    private ExampleTest() { }
+
+    public static void main(final String[] args) {
         int sum = 2 + 2;
         if (sum != 4) {
             throw new AssertionError("Test failed: 2 + 2 != 4");
