9cbcbd73aa729e30830270dd0bc108a12d3fa3d1
jannesurborg <janne.surborg@hotmail.de>
2025-11-13 19:52:43 +0100

    user entity hinzugefügt

--- src/main/java/com/example/cityfeedback/usermanagement/infrastructure/UserRepository.java
+++ src/main/java/com/example/cityfeedback/usermanagement/infrastructure/UserRepository.java
@@ -2,10 +2,11 @@ package com.example.cityfeedback.usermanagement.infrastructure;
 
 import com.example.cityfeedback.usermanagement.domain.valueobjects.Email;
 import com.example.cityfeedback.usermanagement.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(Email email);
 
-    void save(User user);
+    //void save(User user);
 }
