c2210bac49a24871d864d6af8b0830d1c3771a44
jannesurborg <janne.surborg@hotmail.de>
2025-11-13 19:47:36 +0100

    usercontroller hinzugefügt

--- src/main/java/com/example/cityfeedback/usermanagement/application/UserController.java
+++ src/main/java/com/example/cityfeedback/usermanagement/application/UserController.java
@@ -1,9 +1,34 @@
 package com.example.cityfeedback.usermanagement.application;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cityfeedback.feedbackmanagement.application.FeedbackService;
+import com.example.cityfeedback.feedbackmanagement.domain.model.Feedback;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return this.userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return this.UserService.getUserById(id);
+    }
+
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return this.userService.createUser(user);
+    }
 }
