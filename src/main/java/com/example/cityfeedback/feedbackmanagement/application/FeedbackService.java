6b8705f46de680f421ff198c64fb9f598a529376
jannesurborg <janne.surborg@hotmail.de>
2025-11-13 19:16:11 +0100

    feedback service hinzugefügt

--- src/main/java/com/example/cityfeedback/feedbackmanagement/application/FeedbackService.java
+++ src/main/java/com/example/cityfeedback/feedbackmanagement/application/FeedbackService.java
@@ -1,4 +1,33 @@
 package com.example.cityfeedback.feedbackmanagement.application;
 
+import com.example.cityfeedback.feedbackmanagement.domain.model.Feedback;
+import com.example.cityfeedback.feedbackmanagement.infrastructure.FeedbackRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FeedbackService {
+
+    private final FeedbackRepository feedbackRepository;
+
+    public FeedbackService(FeedbackRepository feedbackRepository) {
+        this.feedbackRepository = feedbackRepository;
+    }
+
+    public List<Feedback> getAllFeedbacks() {
+        return this.feedbackRepository.findAll();
+    }
+
+    public Feedback getFeedbackById(Long id) {
+        if(!this.feedbackRepository.existsById(id)) {
+            throw new EntityNotFoundException("no feedback with given id found");
+        }
+        return this.feedbackRepository.findById(id).get();
+    }
+
+    public Feedback createFeedback(Feedback feedback) {
+        return this.feedbackRepository.save(feedback);
+    }
 }
